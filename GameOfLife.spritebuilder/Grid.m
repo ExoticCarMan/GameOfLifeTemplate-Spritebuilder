82705c4b2ffb42c35b6cff335be046d5e37a7eca
ExoticCarMan <nathancarguy@gmail.com>
2014-07-08 22:04:41 -0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -146,9 +146,6 @@ static const int GRID_COLUMNS = 10;
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
             
-            // remember that every creature has a 'livingNeighbors' property that we created earlier
-            currentCreature.livingNeighbors = 0;
-            
             // now examine every cell around the current one
             
             // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
